b07b1f579a7cd4006a6282620ddd3093e50e2f8c
Divyanth Jayaraj <divyanthj@gmail.com>
2018-06-18 21:35:28 +0530

    Added input button and return relevant data

--- src/components/app.js
+++ src/components/app.js
@@ -6,26 +6,39 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      venues : []
+      venues : [],
+      participants: [],
+      address: ''
     };
   }
 
   componentDidMount() {
     console.log("Initialized");
-    axios.get('https://api.foursquare.com/v2/venues/explore', {
-      method: 'GET',
-      params: {
-        client_id: api.id,
-        client_secret: api.secret,
-        ll: '40.7243,-74.0018',
-        near: "Bangalore",
-        query: 'lunch',
-        v: '20180323',
-        limit: 3
-      }
-    }).then((res) => {
-      this.makeData(res.data.response.groups[0].items);
-    })
+    this.fetchData();
+  }
+
+  fetchData() {
+    if(this.state.address.length > 0) {
+      axios.get('https://api.foursquare.com/v2/venues/explore', {
+        method: 'GET',
+        params: {
+          client_id: api.id,
+          client_secret: api.secret,
+          ll: '40.7243,-74.0018',
+          near: this.state.address,
+          query: 'lunch',
+          v: '20180323',
+          limit: 3
+        }
+      }).then((res) => {
+        this.makeData(res.data.response.groups[0].items);
+      })
+    }
+
+  }
+
+  handleInputChange(event) {
+    this.setState({address: event.target.value})
   }
 
   makeData(data) {
@@ -45,9 +58,19 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div></div>
-    )
+      return (
+        <div class="lunchplace">
+          <h2>Lunchplace</h2>
+          <div class='ui input'>
+            <input placeholder='Address' class='ui input' value={this.state.address}  onChange={this.handleInputChange.bind(this)}/>
+          </div>
+          <button onClick={this.fetchData.bind(this)} class='ui button'>Get Venues</button>
+          <table>
+          </table>
+        </div>
+      )
+    }
+
   }
 }
 
